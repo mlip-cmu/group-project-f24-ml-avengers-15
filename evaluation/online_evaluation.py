ca2c588e1483bed1eda2095a53da78e0515db326
priyangadurairaj <pduraira@andrew.cmu.edu>
2024-10-28 20:27:29 -0400

    changed path

--- evaluation/online_evaluation.py
+++ evaluation/online_evaluation.py
@@ -1,7 +1,7 @@
 import traceback
 import pandas as pd
 from app import recommend_movies
-from config import DATA_PATH
+from config import DATA_DIR
 
 # Calculate Precision@K
 def calculate_precision_at_k(user_recommendations, user_relevant_movies, k=10):
@@ -22,9 +22,9 @@ def calculate_precision_at_k(user_recommendations, user_relevant_movies, k=10):
 def get_user_relevant_movies(df, rating_threshold=4):
     return df[df['rating'] >= rating_threshold].groupby('user_id')['movie_id'].apply(set).to_dict()
 
-def evaluate_snapshot(DATA_PATH: str):
+def evaluate_snapshot(DATA_DIR: str):
     try:
-        snapshot_df = pd.read_csv(DATA_PATH)
+        snapshot_df = pd.read_csv(DATA_DIR)
         user_relevant_movies = get_user_relevant_movies(snapshot_df)
         user_recommendations = {user_id: recommend_movies(user_id) for user_id in user_relevant_movies.keys()}
         
