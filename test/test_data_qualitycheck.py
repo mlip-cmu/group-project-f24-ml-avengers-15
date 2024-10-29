8a121db4c3b707488ed7f00e3cca71da2e353ae2
priyangadurairaj <pduraira@andrew.cmu.edu>
2024-10-28 21:42:12 -0400

    Added Data Q part

--- test/test_data_qualitycheck.py
+++ test/test_data_qualitycheck.py
@@ -7,15 +7,8 @@ import pytest
 # Adjust the import path as necessary based on your project structure
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from data_qualitycheck import (
-    check_timestamp_format,
-    validate_user_id_format,
-    check_watch_action,
-    validate_rating_value,
-    validate_recommend_result_structure,
-    verify_action_schema,
-    assess_log_entry,
-)
+from data_qualitycheck import check_timestamp_format, validate_user_id_format, check_watch_action, validate_rating_value, validate_recommend_result_structure,verify_action_schema,assess_log_entry
+
 
 # Timestamp validation tests
 @pytest.mark.parametrize("timestamp,expected", [
